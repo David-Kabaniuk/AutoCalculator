c36e24acc4b0481ebd3135651d59a15f3a555f6a
David <Gkom66965@gmail.com>
2023-08-04 18:56:35 +0300

    BASE Done %80 FIXET''first_num = int(example_int[0]) ValueError: invalid literal for int() with base 10: '' LINE 36 MAKS HELP

--- auto_calculator.py
+++ auto_calculator.py
@@ -12,9 +12,29 @@ def split_file_in_line(file_no_in_line):
     return text_in_line
 
 
-def math(file_int):
-    for example in file_int:
-        print(example)
+def function_split(file_need_split):
+    file_rename = (file_need_split.replace(' ', ';'))
+    file_split = file_rename.split(';')
+    return file_split
+
+
+def math(file_str):
+    for line_str in file_str:
+        example_str = line_str[3::]
+        example_int = function_split(example_str)
+        first_num = int(example_int[0])
+        two_num = int(example_int[2])
+        if example_int[1] == '+':
+            result = first_num + two_num
+        elif example_int[1] == '-':
+            result = first_num - two_num
+        elif example_int[1] == '/':
+            result = first_num / two_num
+        elif example_int[1] == '*':
+            result = first_num * two_num
+
+        print(result)
+
 
 
 math(split_file_in_line(read_file(file_path)))
